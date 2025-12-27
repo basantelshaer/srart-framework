c5e7d80fe524d97b17ab4415e82b59cfcd08fbda
basantelshaer <basantredaelshaer@gmail.com>
2025-12-27 19:02:21 +0400

    fix assets path

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import img1 from '../../Assets/port1.png'
-import img2 from '../../Assets/port2.png'
-import img3 from '../../Assets/port3.png'
+import img1 from '../../assets/port1.png'
+import img2 from '../../assets/port2.png'
+import img3 from '../../assets/port3.png'
 import './portfolio.css'
 
 export default function Portfolio() {
